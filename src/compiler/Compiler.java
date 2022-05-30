521e757457ca77c5e791104ac02bfaedd4ba32aa
lucasAlex-sys <66279406+lucasAlex-sys@users.noreply.github.com>
2022-05-29 22:44:00 -0300

    Update Compiler

--- src/compiler/Compiler.java
+++ src/compiler/Compiler.java
@@ -7,11 +7,12 @@ import java.io.IOException;
 public class Compiler {
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
+        File path = new File(System.getProperty("user.dir")); 
         if(args.length==0){
             System.out.println("Necessario arquivo como parametro");
             System.exit(-1);
         }
-        LexicalAnalyser lexicalAnalyser = new LexicalAnalyser(args[0]);
+        LexicalAnalyser lexicalAnalyser = new LexicalAnalyser(path+"\\"+args[0]);
         TOKEN token;
         System.out.println("**** TOKENS ****");
         System.out.println();
